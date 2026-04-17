e852fce64877ab5ffd8f6a36d33ae1dce5ea88a5
FelipeBerserkRsl <38743962+FelipeBerserkRsl@users.noreply.github.com>
2026-04-17 16:40:24 -0300

    feat: add initial route handlers for public and private APIs

---
+++ TaskManager/routes/private.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/tasks', (req, res) => {
+});
+
+router.get('/tasks', (req, res) => {
+});
+
+router.get('/tasks/:id', (req, res) => {
+});
+
+router.put('/tasks/:id', (req, res) => {
+});
+
+router.delete('/tasks/:id', (req, res) => {
+});
+
+export default router;
--- TaskManager/server.js
+++ TaskManager/server.js
@@ -3,7 +3,16 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+import publicRoutes from './routes/public.js';
+import privateRoutes from './routes/private.js';
+
+app.use('/public', publicRoutes);
+app.use('/private', privateRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
